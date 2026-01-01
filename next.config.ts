87cd0e1a6c9da5f73d55e723b107c953c1e9b3e8
José Bohopo <jr.bohopo@gmail.com>
2026-01-01 19:19:30 +0100

    feature: install worker-loader and config next.config.ts for web worker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.modules.rules.push({
+        test: /\.worker\.(ts|js)$/,
+        loader: 'worker-loader',
+        options: {
+          filename: 'static/[hash].worker.js',
+          publicPath: '/_next/',
+        }
+      })
+    }
+    
+    return config;
+  }
 };
 
 export default nextConfig;
